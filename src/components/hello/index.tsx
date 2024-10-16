33e1ac12555bf7642378485744115fd78b0c768f
xun082 <2042204285@qq.com>
2024-10-16 12:38:39 +0800

    feat: 🚀 initialize repository with basic configurations

---
+++ src/components/hello/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface HelloProps {
+  name: string;
+}
+
+const Hello: React.FC<HelloProps> = ({ name }) => {
+  return <h1>Hello, {name}!</h1>;
+};
+
+export default Hello;
